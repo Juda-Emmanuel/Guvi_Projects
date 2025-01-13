56592191c6e5b820783762e4721d6ff54036595f
Juda Emmanuel <131518624+Juda-Emmanuel@users.noreply.github.com>
2025-01-13 19:11:36 +0530

    Add files via upload

---
+++ DemoBlaze_Project/src/test/java/DemoBlaze_Project/DemoBlaze_Project/Base_Test.java
@@ -0,0 +1,32 @@
+package DemoBlaze_Project.DemoBlaze_Project;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Ignore;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base_Test {
+
+	protected WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.demoblaze.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
